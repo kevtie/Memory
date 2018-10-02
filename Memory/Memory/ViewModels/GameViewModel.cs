b9a390fbe81b9465515b144e011827b2820727f9
Boele <boele.boom@hotmail.com>
2018-10-02 14:51:18 +0200

    Add basic grid functionality without MVVM

---
+++ Memory/Memory/ViewModels/GameViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Memory.ViewModels
+{
+    public class GameViewModel
+    {
+        public int columns = 4;
+        public int rows = 4;
+
+        public List<Object> Columns {
+            get
+            {
+                var list = new List<Object>();
+                for (int i = 0; i < columns; i++)
+                {
+                    list.Add(SetColumnDetails(new ColumnDefinition()));
+                }
+                return list;
+            }
+        }
+
+        public List<Object> Rows
+        {
+            get
+            {
+                var list = new List<Object>();
+                for (int i = 0; i < rows; i++)
+                {
+                    list.Add(SetRowDetails(new RowDefinition()));
+                }
+                return list;
+            }
+        }
+
+        public GameViewModel()
+        {
+
+        }
+
+        private object SetColumnDetails(ColumnDefinition column)
+        {
+            column.Width = new GridLength('*', GridUnitType.Star);
+
+            return column;
+        }
+
+        private object SetRowDetails(RowDefinition row)
+        {
+            row.Height = new GridLength('*', GridUnitType.Star);
+
+            return row;
+        }
+    }
+}
---
+++ Memory/Memory/Views/GameView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Memory.ViewModels;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Memory.Views
+{
+    /// <summary>
+    /// Interaction logic for GameView.xaml
+    /// </summary>
+    public partial class GameView : Window
+    {
+        public GameView()
+        {
+            InitializeComponent();
+            CreateGrid(10, 10);
+        }
+
+        public void CreateGrid(int columns, int rows)
+        {
+            for (int i = 0; i < columns; i++)
+            {
+                Grid.ColumnDefinitions.Add(SetColumnDetails(new ColumnDefinition()));
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                Grid.RowDefinitions.Add(SetRowDetails(new RowDefinition()));
+            }
+        }
+
+        public ColumnDefinition SetColumnDetails(ColumnDefinition column)
+        {
+            column.Width = new GridLength(30, GridUnitType.Star);
+
+            return column;
+        }
+
+        private RowDefinition SetRowDetails(RowDefinition row)
+        {
+            row.Height = new GridLength(50, GridUnitType.Star);
+
+            return row;
+        }
+    }
+}
