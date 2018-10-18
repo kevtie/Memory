a6d4e08df0ee2dedae22e6524d67af8921b13aa6
Tim Miedema <tim.miedema@student.nhlstenden.com>
2018-10-18 14:01:31 +0200

    iets weet niet wat anders is

--- Memory/Memory v2/MainWindow.xaml.cs
+++ Memory/Memory v2/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Shapes;
 
 namespace Memory_v2
 {
-    /// <summary>
+    /// <summary> 
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
