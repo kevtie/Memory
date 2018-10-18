8106cd9f079d449f32e1416b78b0bae7781e81f3
stevohoog <43113400+stevohoog@users.noreply.github.com>
2018-10-18 10:34:56 +0100

    Add files via upload

---
+++ Wpfdemo2/Wpfdemo2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpfdemo2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Score> myScore { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            myScore = new List<Score>();
+
+            Score score1 = new Score();
+            score1.Name = "jan";
+            score1.score = 9001;
+
+            Score score2 = new Score();
+            score2.Name = "Karel";
+            score2.score = 8999;
+
+            Score score3 = new Score();
+            score3.Name = "piet";
+            score3.score = 8998;
+
+
+            myScore.Add(score1);
+            myScore.Add(score2);
+            myScore.Add(score3);
+
+            DataContext = this;
+        }
+    }
+}
---
+++ Wpfdemo2/Wpfdemo2/Score.cs
@@ -0,0 +1,11 @@
+﻿namespace Wpfdemo2
+{
+    public class Score
+    {
+        public string nummer { get; internal set; }
+    
+        public int score { get; set; }
+
+        public string Name { get; internal set; }
+    }
+}
