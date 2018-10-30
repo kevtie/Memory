ec97c3e54c3d8df5051c79dfe690460518cd7fab
boelenhl <boele.boom@student.nhlstenden.com>
2018-10-30 20:56:27 +0100

    Add Position documentation

--- Memory/Memory/Position.cs
+++ Memory/Memory/Position.cs
@@ -1,8 +1,16 @@
+/// <summary>
+/// Player class is a class where a positions data is stored.
+/// </summary>
 public class Position
 {
     public int X;
     public int Y;
 
+    /// <summary>
+    /// Position is a method that gets excecuted when a new Position object is created.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
     public Position(int x, int y)
     {
         X = x;
