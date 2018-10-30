58bf2e7100190025880f66e441486c67058f2b42
boelenhl <boele.boom@student.nhlstenden.com>
2018-10-30 21:10:00 +0100

    Add Background documentation

--- Memory/Memory/Background.cs
+++ Memory/Memory/Background.cs
@@ -1,9 +1,18 @@
+/// <summary>
+/// Background is a class where a backgrounds data is stored.
+/// </summary>
 public class Background
 {
     public int Id;
     public string Front;
     public string Back;
 
+    /// <summary>
+    /// Background is a method that gets excecuted when a new Background object is created.
+    /// </summary>
+    /// <param name="id">Unique identifier.</param>
+    /// <param name="back">Card back image path.</param>
+    /// <param name="front">Card front image path.</param>
     public Background(int id, string back, string front)
     {
         Id = id;
